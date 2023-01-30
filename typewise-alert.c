7af16f914626e21fc8def35630a2e25b5ddd5a50
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-01-29 19:02:27 -0600

    Update typewise-alert.c
    adding the functions for the format of the messages

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,5 +1,6 @@
 #include "typewise-alert.h"
 #include <stdio.h>
+#include <string.h>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
@@ -46,7 +47,7 @@ void checkAndAlert(
   }
 }
 
-void sendToController(BreachType breachType) {
+/*void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
 }
@@ -66,5 +67,61 @@ void sendToEmail(BreachType breachType) {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is normal, everything cool :)\n");
       break;
-  }
+  }*/
 }
+
+char* Format_sendToController_Test(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  static char buffer[20]= "0xfeed :";
+  static char strBreachType[9];
+  switch(breachType) {
+    case TOO_LOW:
+	  strBreachType = " TOO_LOW";
+	  strcat(buffer,strBreachType);
+      break;
+    case TOO_HIGH:
+	  strBreachType = " TOO_HIGH";
+	  strcat(buffer,strBreachType);
+      break;
+    default:
+	  strBreachType = " NORMAL";
+	  strcat(buffer,strBreachType);
+      break;
+  }
+  
+  return buffer;
+}
+
+void sendToController(BreachType breachType) {
+  printf("%x",Format_sendToController_Test(breachType));
+}
+
+char * Format_sendToEmail(BreachType breachType)
+{
+  //const char* recepient = "a.b@c.com";
+  static char message[70]= "To: a.b@c.com\n";
+  static char alarm[50];
+  
+  switch(breachType) {
+    case TOO_LOW:
+	    alarm = "Hi, the temperature is too low\n";
+	    strcat(message,alarm);
+      break;
+    case TOO_HIGH:
+      alarm = "Hi, the temperature is too high\n";
+	    strcat(message,alarm);
+      break;
+    default:
+	    alarm = "Hi, the temperature is normal, everything cool :)\n";
+	    strcat(message,alarm);
+      break;
+  }
+  return message;
+}
+
+
+void sendToEmail(BreachType breachType) {
+  printf("%x",Format_sendToEmail(breachType));
+}
+
+
